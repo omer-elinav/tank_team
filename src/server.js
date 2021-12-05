f745be8f2d71df0b937250316b50b340e31d9ed8
Snarkface <tomtomkan@gmail.com>
2021-12-05 20:48:41 +0200

    Add get index.js

--- src/server.js
+++ src/server.js
@@ -4,15 +4,19 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/index.js', (req, res) => {
+    res.sendFile(__dirname + '/index.js');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
+    socket.on('chat message', msg => {
     io.emit('chat message', msg);
-  });
+    });
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
