68cdab65442008cfa7d4d89c7863e7a00563b127
Snarkface <tomtomkan@gmail.com>
2021-12-05 23:46:04 +0200

    Add update event to client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import {SimpleButton} from "./SimpleButton.js";
 
 const Application = PIXI.Application;
+const socket = io();
 
+var game_date = {
+    map: [],
+    players: [],
+    events: []
+};
 
 const app = new Application({
     view: document.getElementById("pixi-canvas"),
@@ -10,6 +16,12 @@ const app = new Application({
     backgroundColor: 0x6495ed,
     width: 640,
     height: 480
+});
+
+// Event from server
+socket.on('update', data => {
+    // Update logic goes here
+    game_data = data;
 });
 
 const simple_button = new SimpleButton(app.screen.width, app.screen.height);
--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 3000;
 // Game status is stored here
 // Change as needed
 var game_status = {
-    map: "",
-    players: "",
-    events: ""
+    map: [],
+    players: [],
+    events: []
 };
 
 // All the http requests are handled here
@@ -24,14 +24,16 @@ app.get('/*', (req, res) => {
 io.on('connection', (socket) => {
     // Event from client 
     socket.on('update', data => {
-    // Update logic goes here
-    console.log("hey");
-    game_status = data;
-    // Broadcast update to everyone
-    io.emit('update', game_status);
-    /* In order to respond to this client
-       socket.emit() should used instead
-    */
+        // Update logic goes here
+        // TODO: make sure only relevant player data is updated
+        //       in order to avoid race condition
+        console.log("hey");
+        game_status = data;
+        // Broadcast update to everyone
+        io.emit('update', game_status);
+        /* In order to respond to this client
+        socket.emit() should used instead
+        */
     });
 });
 
